16761d3e786e7e7a3fa1e7c19369fd91441f2023
Aryan Jadon <aryan.jadon@sjsu.edu>
2022-05-13 11:09:24 -0700

    Added CDN File PAth

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ import numpy as np
 
 st.title('AlternusVera Dashboard')
 
+cdn_repo = "https://raw.githubusercontent.com/aryan-jadon/machine-learning-dashboard/master/cdn_model_weights/{file_name}.pkl"
+
 process_function = st.selectbox('Choose an Action', ['None', 'Process New News'])
 
 if process_function == 'Process New News':
     st.header('Collecting News from Sources')
     st.subheader('Description')
-    st.write('Test Write')
-    st.markdown(r'Test Write')
-    st.write('Test Write')
+    st.write('Loading Model Weights')
+    naive_bayes_weights = cdn_repo.format(file_name="NaiveBayes")
+    st.write('Loading Naive Bayes Weights')
+    SVM_weights = cdn_repo.format(file_name="SVM")
+    st.write('Loading SVM Weights')
 
