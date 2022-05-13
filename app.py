960eeea23ae79ea76124494b8a5e07617214298d
Aryan Jadon <aryan.jadon@sjsu.edu>
2022-05-13 11:14:10 -0700

    Added News Sources

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 import streamlit as st
+from news_articles import extract_rss_feeds
 import numpy as np
-
+import json
 
 st.title('AlternusVera Dashboard')
 
-cdn_repo = "https://raw.githubusercontent.com/aryan-jadon/machine-learning-dashboard/master/cdn_model_weights/{file_name}.pkl"
+cdn_repo = "https://raw.githubusercontent.com/aryan-jadon/machine-learning-dashboard/master/cdn_model_weights/{" \
+           "file_name}.pkl "
 
 process_function = st.selectbox('Choose an Action', ['None', 'Process New News'])
 
 if process_function == 'Process New News':
     st.header('Collecting News from Sources')
+    st.write('Loading Model Weights')
+    news_sources = json.load("rss/news_sources.json")
+    st.write(news_sources)
+    # st.dataframe(df)
+
     st.subheader('Description')
     st.write('Loading Model Weights')
     naive_bayes_weights = cdn_repo.format(file_name="NaiveBayes")
