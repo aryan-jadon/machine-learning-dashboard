24ba2a26379aaf3768dca5d2710d01a6f5ae2682
Aryan Jadon <aryan.jadon@sjsu.edu>
2022-05-16 13:33:22 -0700

    added file

--- app.py
+++ app.py
@@ -10,15 +10,9 @@ cdn_repo = "https://raw.githubusercontent.com/aryan-jadon/machine-learning-dashb
 cdn_json = "https://raw.githubusercontent.com/aryan-jadon/machine-learning-dashboard/master/rss/news_sources.json"
 
 process_function = st.selectbox('Choose an Action', ['None',
-                                                     'Gather News from RSS Feed',
-                                                     'Classify News'])
+                                                     'Run Analysis'])
 
-if process_function == 'Gather News from RSS Feed':
+if process_function == 'Run Analysis':
     st.header('Collecting News from Sources')
-
     news_file = pd.read_excel("https://machine-learning-articles.s3.us-west-2.amazonaws.com/05-16-2022.xlsx")
-    st.table(news_file)
-
-
-if process_function == 'Classify News':
-    st.header('Processing Dataframe')
+    st.dataframe(news_file)
